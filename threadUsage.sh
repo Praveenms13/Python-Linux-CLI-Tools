3b798981e47a5e4e8c11262e1f1b7a6d7b639077
praveen <mspraveenkumar77@gmail.com>
2023-03-18 04:18:50 +0000

    added_Thread_and_MultiProcess

---
+++ threadUsage.sh
@@ -0,0 +1,6 @@
+# Same Usage for multiprocess
+
+python3 thread
+Enter url: https://thumbs.dreamstime.com/b/beautiful-sunset-over-water-tree-silhouette-nature-landscape-amazing-orange-yellow-sky-night-scene-wallpaper-birds-flying-154424473.jpg
+Enter Output File name: sample1.jpg
+Downloading....
