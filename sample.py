84826c727af80e23a4e50182ba338dc930bf04b6
praveen <mspraveenkumar77@gmail.com>
2023-03-31 00:26:50 +0000

    update

--- sample.py
+++ sample.py
@@ -1,6 +1,7 @@
 import threading
+import pandas as pd
+import time
 import mysql.connector
-# pip install mysql-connector-python
 cnx = mysql.connector.connect(
     host="mysql.selfmade.ninja",
     user="iorProject",
@@ -16,15 +17,24 @@ def toMySql(dt, pmt_2_5, pmt_10, aqi_2_5, aqi_10):
     cursor.close()
     cnx.close()
 
-def read_sensor_data():
-    sensor_data1 = "data1"
-    sensor_data2 = "data2"
-    sensor_data3 = "data3"
-    sensor_data4 = "data4"
-    sensor_data5 = "data5"
+def save_log():
+    with open('Air_pon.csv', 'a') as log:
+        pmt_2_5, pmt_10 = get_data()
+        aqi_2_5, aqi_10 = conv_aqi(pmt_2_5, pmt_10)
+        dt = datetime.now()
+        log.write("{}, {}, {}, {}, {}\n".format(dt, pmt_2_5, pmt_10, aqi_2_5, aqi_10))
+        sensor_data = (dt, pmt_2_5, pmt_10, aqi_2_5, aqi_10)
+        log.close()
 
+def threadFunc():
+    t1 = threading.Thread(target=save_log)
+    t1.start()
+    sensor_data1 = "dt"
+    sensor_data2 = "pmt_2_5"
+    sensor_data3 = "pmt_10"
+    sensor_data4 = "aqi_2_5"
+    sensor_data5 = "aqi_10"
     t2 = threading.Thread(target=toMySql, args=(sensor_data1, sensor_data2, sensor_data3, sensor_data4, sensor_data5))
     t2.start()
 
-if __name__ == '__main__':
-    read_sensor_data()
+
